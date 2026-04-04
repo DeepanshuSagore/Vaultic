6fb69bd682f9d811ea3590ea92231bf30a95be3d
Trash <deepanshusagore220128@acropolis.in>
2026-04-04 21:25:44 +0530

    a few fixes

--- server/db/connect.js
+++ server/db/connect.js
@@ -7,7 +7,13 @@ export async function connectDatabase() {
     throw new Error('Missing MONGODB_URI in environment variables')
   }
 
+  console.log(
+    'Connecting to MongoDB:',
+    mongoUri.replace(/\/\/.*@/, '//***:***@'),
+  )
+
   const dbName = process.env.MONGODB_DB_NAME || undefined
 
-  await mongoose.connect(mongoUri, { dbName })
+  await mongoose.connect(mongoUri, { dbName, serverSelectionTimeoutMS: 10000 })
+  console.log('MongoDB connected successfully')
 }
